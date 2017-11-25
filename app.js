aeef560fa73dbe7817f80018bee3dcb9fcd20bdf
Kenneth Brisco <kennethbrisco1@gmail.com>
2017-11-25 04:05:14 -0700

    handled auth

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const mongooose = require('mongoose');
+const passport  = require('passport');
+// Passport Config
+require('./config/passport')(passport);
 
+// Load Routes
+const auth = require('./routes/auth');
 
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('it works');
 });
+
+// Use Routes
+app.use('/auth', auth);
 
 const port = process.env.PORT || 5000;
 
---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI: 'mongodb://<dbuser>:<dbpassword>@ds119486.mlab.com:19486/storybooks-dev',
+  googleClientID: '765392280625-1s99qi9rrjkeo2f1guvhnqtlbnaj09m2.apps.googleusercontent.com',
+  googleClientSecret: 'NDu2eTwU_Uiig-QOR1SqvA8s'
+}
